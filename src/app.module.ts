e3344929bcb2726e9a620d4c30ce7d5e291aa95f
Dylan Dinh <ngocduydinh2000@gmail.com>
2025-08-03 23:21:08 +0700

    feat: setting up controller, module, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'your_username',
+      password: 'your_password',
+      database: 'your_database',
+      entities: [],
+      synchronize: true, // set to false in production
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
