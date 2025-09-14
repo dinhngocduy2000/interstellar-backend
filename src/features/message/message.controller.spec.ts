d9159b5ac55d30f234f07364c36e6365ea898a39
Dylan Dinh <ngocduydinh2000@gmail.com>
2025-09-14 21:14:12 +0700

    fix: adding env to docker compose

--- src/features/message/message.controller.spec.ts
+++ src/features/message/message.controller.spec.ts
@@ -1,7 +1,7 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { MessageController } from './message.controller';
+import { Test, TestingModule } from "@nestjs/testing";
+import { MessageController } from "./message.controller.js";
 
-describe('MessageController', () => {
+describe("MessageController", () => {
   let controller: MessageController;
 
   beforeEach(async () => {
@@ -12,7 +12,7 @@ describe('MessageController', () => {
     controller = module.get<MessageController>(MessageController);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(controller).toBeDefined();
   });
 });
