4b4e91152cfec35135686fd0b5eba45cde1ed84a
Dylan Dinh <ngocduydinh2000@gmail.com>
2025-09-24 23:58:25 +0700

    feat: private chat

--- src/dto/message/message-request.dto.ts
+++ src/dto/message/message-request.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class MessageRequestDTO {
   @ApiProperty({
@@ -9,4 +9,8 @@ export class MessageRequestDTO {
   @IsNotEmpty()
   @IsString()
   content: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isPrivate?: boolean;
 }
